0c7324d326f98fd3fd7df2ac7df950d3fef78ac6
Emre Kılıç <emre1kilic@outlook.com>
2022-12-12 21:47:12 +0300

    add day #1

---
+++ CalorieCounting.cs
@@ -0,0 +1,33 @@
+﻿// DAY 1
+
+internal class CalorieCounting
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(Count());
+    }
+
+    static int Count()
+    {
+        int maxPtr1 = 0;
+        int maxPtr2 = 0;
+        // Read the file and display it line by line.
+        foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+        {
+            if (line == "")
+            {
+                if (maxPtr1 > maxPtr2)
+                {
+                    maxPtr2 = maxPtr1;
+                    System.Console.WriteLine(maxPtr1);
+                }
+                maxPtr1 = 0;
+            }
+            else
+            {
+                maxPtr1 += Int32.Parse(line);
+            }
+        }
+        return maxPtr2;
+    }
+}
