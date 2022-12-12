cf1e9d023b310a1bcfdbdc49f33da319d2c45d87
Emre Kılıç <emre1kilic@outlook.com>
2022-12-12 22:09:15 +0300

    add part 2 - day #1

--- CalorieCounting.cs
+++ CalorieCounting.cs
@@ -4,7 +4,37 @@ internal class CalorieCounting
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine(Count());
+        System.Console.WriteLine(FindTopThree());
+    }
+
+    static int FindTopThree()
+    {
+        int maxPtr1 = 0;
+        // int maxPtr2 = 0;
+        var l = new List<int>();
+
+        // Read the file and display it line by line.
+        foreach (string line in System.IO.File.ReadLines(@"input.txt"))
+        {
+            if (line == "")
+            {
+                l.Add(maxPtr1);
+                maxPtr1 = 0;
+            }
+            else
+            {
+                maxPtr1 += Int32.Parse(line);
+            }
+        }
+
+        l.Sort();
+
+        System.Console.WriteLine(l[l.Count - 1]);
+        System.Console.WriteLine(l[l.Count - 2]);
+        System.Console.WriteLine(l[l.Count - 3]);
+
+        var total = l[l.Count - 1] + l[l.Count - 2] + l[l.Count - 3];
+        return total;
     }
 
     static int Count()
