f2b6125b1829a5d66dc0fa74ccd56b1fbed03507
Emre Kılıç <emre1kilic@outlook.com>
2022-12-13 10:58:51 +0300

    finish part 2 - day 2

--- RockPaperScissors.cs
+++ RockPaperScissors.cs
@@ -4,7 +4,7 @@ public class RockPaperScissors
 {
     static void Main(string[] args)
     {
-        int r = StrategyGuide();
+        int r = Part2();
         System.Console.WriteLine(r);
     }
 
@@ -83,6 +83,88 @@ public class RockPaperScissors
                 {
                     // win
                     totalScore += 2;
+                }
+            }
+        }
+
+        return totalScore;
+    }
+
+    static int Part2()
+    {
+        // First Column
+        // A - Rock - 1
+        // B - Paper - 2
+        // C - Scissors - 3
+
+        // Second Column
+        // X - Rock - 1
+        // Y - Paper - 2
+        // Z - Scissors - 3
+
+        // 0 - Lost
+        // 3 - draw
+        // 6 - win
+
+        int totalScore = 0;
+
+        foreach (string line in System.IO.File.ReadLines(@"inputDay2.txt"))
+        {
+            char col1 = line[0]; // opponent
+            char col2 = line[2]; // player
+
+            if (col1 == 'A') // ROCK
+            {
+                if (col2 == 'X') // lose 
+                {
+                    // SCISSORS
+                    totalScore += 3;
+                }
+                if (col2 == 'Y') // draw
+                {
+                    // ROCK
+                    totalScore += 4;
+                }
+                if (col2 == 'Z') // win
+                {
+                    // PAPER
+                    totalScore += 8;
+                }
+            }
+            if (col1 == 'B') // PAPER
+            {
+                if (col2 == 'X') // lose
+                {
+                    // ROCK
+                    totalScore += 1;
+                }
+                if (col2 == 'Y') // draw
+                {
+                    // PAPER
+                    totalScore += 5;
+                }
+                if (col2 == 'Z') // win
+                {
+                    // SCISSORS
+                    totalScore += 9;
+                }
+            }
+            if (col1 == 'C') // SCISSORS
+            {
+                if (col2 == 'X') // lose
+                {
+                    // PAPER
+                    totalScore += 2;
+                }
+                if (col2 == 'Y') // draw
+                {
+                    // SCISSORS
+                    totalScore += 6;
+                }
+                if (col2 == 'Z') // win
+                {
+                    // ROCK
+                    totalScore += 7;
                 }
             }
         }
